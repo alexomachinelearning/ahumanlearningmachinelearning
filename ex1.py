65045d76680b0bc110f8076f6d99e0133ea3ea8d
Alex Ortiz <alexomachinelearning@gmail.com>
2019-01-11 13:39:44 -0800

    Add pythonlearning

---
+++ ex1.py
@@ -0,0 +1,11 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# Excercise from "Excercise 1: A Good First Program" by Learn Python the Hard Way.
+# I learned how to create a python file, edit it in Atom, type the code above, and run python in Terminal to print this code.
+# Other things that helped me: looking up how to use the commands "sed" and "awk" to print specific line(s) of code from a python file.
