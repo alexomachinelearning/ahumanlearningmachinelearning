ab3d226362cb21f4643ace4d02126082fd5378e2
Alex Ortiz <alexomachinelearning@gmail.com>
2019-01-11 17:59:14 -0800

    Add new directory for LPTHW exercises, rename ex1 to ex01 for easy future sorting

---
+++ LPTHW/ex02.py
@@ -0,0 +1,17 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+print "I could have code like this."
+print "This will run."
+
+# Exercise from "Exercise 2" in Learn Python the Hard Way.
+# I learned the purpose of the octothorpe.
+# I observed that print "message" and print("message") both lead to the same outcome when running python.
+# It's an interesting juxtaposition that an octophorpe is used in code to hide something yet is used for hashtags on social media platforms to find and show something to a wider audience.
